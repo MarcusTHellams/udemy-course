03b0a5e5ef4c548738e2067555d114ef4f4b8e41
Marcus Hellams <mhellams@hotmail.com>
2021-06-20 23:10:20 -0400

    set up sqlite db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
+console.log(join(__dirname, '../../', 'udemy-course.db'));
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+      database: `${join(__dirname, '../../', 'udemy-course.db')}`,
+      synchronize: true,
+      type: 'sqlite',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
