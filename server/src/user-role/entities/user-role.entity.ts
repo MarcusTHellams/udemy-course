4ce7cb6562e77a5e74ff555b646272c5c1c6f457
Marcus Hellams <mhellams@hotmail.com>
2021-06-22 16:59:33 -0400

    on Delete

--- server/src/user-role/entities/user-role.entity.ts
+++ server/src/user-role/entities/user-role.entity.ts
@@ -10,11 +10,17 @@ export class UserRole {
   @PrimaryColumn('varchar', { nullable: false })
   roleId: string;
 
-  @ManyToOne(() => User, (user) => user.roles, { primary: true })
+  @ManyToOne(() => User, (user) => user.roles, {
+    primary: true,
+    onDelete: 'CASCADE',
+  })
   @JoinColumn({ name: 'userId' })
   user: User;
 
-  @ManyToOne(() => Role, (role) => role.users, { primary: true })
+  @ManyToOne(() => Role, (role) => role.users, {
+    primary: true,
+    onDelete: 'CASCADE',
+  })
   @JoinColumn({ name: 'roleId' })
   role: Role;
 }
