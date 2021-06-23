ce2ff0b7783ca859809f3ab46d42d66c65647a8a
Marcus Hellams <mhellams@hotmail.com>
2021-06-23 10:01:42 -0400

    update header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -10,19 +10,24 @@ import {
 } from '@chakra-ui/react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
+type HeaderProps = {
+  [key: string]: any;
+};
 
-export const Header = (props: {}): JSX.Element => {
+export const Header = (props: HeaderProps): JSX.Element => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const handleToggle = () => (isOpen ? onClose() : onOpen());
   return (
     <>
       <Flex
+        boxShadow="lg"
+        position="sticky" 
         as='nav'
         align='center'
         justify='space-between'
         wrap='wrap'
         padding={6}
-        bg='teal.500'
+        bg='red.800'
         color='white'
         {...props}
       >
@@ -32,7 +37,7 @@ export const Header = (props: {}): JSX.Element => {
           </Heading>
         </Flex>
 
-        <Box display={{ base: 'block', md: 'none' }} onClick={handleToggle}>
+        <Box cursor="pointer" display={{ base: 'block', md: 'none' }} onClick={handleToggle}>
           <GiHamburgerMenu />
         </Box>
 
