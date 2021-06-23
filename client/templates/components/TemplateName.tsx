99562d0fcd118fe20645136f58ee448bc3a3323a
Marcus Hellams <mhellams@hotmail.com>
2021-06-23 09:52:29 -0400

    init client add header

---
+++ client/templates/components/TemplateName.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+type TemplateNameProps = {};
+
+export const TemplateName = (props: TemplateNameProps): JSX.Element => {
+  return (
+    <>
+      <h1>TemplateName component</h1>
+    </>
+  );
+};
