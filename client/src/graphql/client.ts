e3b8c33d5983a4a404b8c0fc1195f5c80e9bdafe
Marcus Hellams <mhellams@hotmail.com>
2021-06-25 10:11:28 -0400

    turn off cache in apollo

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,10 +1,26 @@
 import {
   ApolloClient,
   InMemoryCache,
+  DefaultOptions
 } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+}
+
 
 export const client = new ApolloClient({
   uri: 'http://localhost:5001/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
+  defaultOptions,
 });
 
