84f098df2571713ef5791b9ed54aa46b01762df8
Marcus Hellams <mhellams@hotmail.com>
2021-07-11 23:06:36 -0400

    upkeep

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,26 +1,26 @@
 import {
   ApolloClient,
   InMemoryCache,
-  DefaultOptions,
   HttpLink,
-} from '@apollo/client';
+  DefaultOptions,
+} from "@apollo/client";
+
 
 const defaultOptions: DefaultOptions = {
   watchQuery: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'none',
+    fetchPolicy: "no-cache",
+    errorPolicy: "none",
   },
   query: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'none',
+    fetchPolicy: "no-cache",
+    errorPolicy: "none",
   },
 };
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:5001/graphql',
-  credentials: 'include'
+  uri: "http://localhost:5001/graphql",
+  credentials: "include",
 });
-
 
 export const client = new ApolloClient({
   link: httpLink,
