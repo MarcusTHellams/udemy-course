5f95c7080366eaf9c3aad0d042a10f0207fba7a4
Marcus Hellams <mhellams@hotmail.com>
2021-07-15 14:11:30 -0400

    Responsive table, responsive table added to task list

---
+++ client/src/components/ResponsiveTable/ResponsiveTable.tsx
@@ -0,0 +1,51 @@
+import { Table, TableProps } from '@chakra-ui/react';
+import { TableProps as ReactTableProps } from 'react-table';
+import * as React from 'react';
+
+type ResponsiveTableProps = {
+  reactTableProps?: ReactTableProps;
+  tableProps?: TableProps;
+};
+
+export const ResponsiveTable = ({
+  children,
+  tableProps,
+  reactTableProps,
+}: React.PropsWithChildren<ResponsiveTableProps>): JSX.Element => {
+  return (
+    <>
+      <Table
+        sx={{
+          'thead tr': {
+            position: ['absolute', null, 'inherit'],
+            left: '-9999px',
+            top: '-9999px',
+          },
+          tr: {
+            border: ['1px solid black', null, 'inherit'],
+            px: [4, null, 'inherit'],
+          },
+          td: {
+            display: ['block', null, 'table-cell'],
+            position: ['relative', null, 'inherit'],
+            paddingLeft: ['50%', null, 'inherit'],
+            '&:before': {
+              top: '4',
+              left: '0.375rem',
+              position: 'absolute',
+              content: 'attr(data-th)":"',
+              fontWeight: 'bold',
+              w: '45%',
+              whiteSpace: 'nowrap',
+              display: [null, null, 'none'],
+            },
+          },
+        }}
+        {...tableProps}
+        {...reactTableProps}
+      >
+        {children}
+      </Table>
+    </>
+  );
+};
--- client/src/components/TaskListComponent/TaskListComponent.tsx
+++ client/src/components/TaskListComponent/TaskListComponent.tsx
@@ -1,13 +1,12 @@
-import * as React from "react";
-import { Task } from "../../types/task.type";
+import * as React from 'react';
+import { Task } from '../../types/task.type';
 import {
   Button,
   VStack,
-  HStack,
+  Stack,
   Text,
   Heading,
   ButtonGroup,
-  Table,
   Thead,
   Tbody,
   Tr,
@@ -18,13 +17,13 @@ import {
   Wrap,
   WrapItem,
   Icon,
-} from "@chakra-ui/react";
-import { Link as RLink } from "react-router-dom";
-import { DeletionVerification } from "../DeletionVerification/DeletionVerification";
-import { client } from "../../graphql/client";
-import { useMutation, useQueryClient } from "react-query";
-import { removeTask } from "../../graphql/mutations/removeTask";
-import { PaginatedResults } from "../../types/paginatedResults.type";
+} from '@chakra-ui/react';
+import { Link as RLink } from 'react-router-dom';
+import { DeletionVerification } from '../DeletionVerification/DeletionVerification';
+import { client } from '../../graphql/client';
+import { useMutation, useQueryClient } from 'react-query';
+import { removeTask } from '../../graphql/mutations/removeTask';
+import { PaginatedResults } from '../../types/paginatedResults.type';
 import {
   useTable,
   usePagination,
@@ -32,10 +31,11 @@ import {
   Row,
   HeaderGroup,
   useSortBy,
-} from "react-table";
-import { Paginated } from "@makotot/paginated";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
-import { DirectionEnum, OrderByType } from "../../types/orderBy.type";
+} from 'react-table';
+import { Paginated } from '@makotot/paginated';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import { DirectionEnum, OrderByType } from '../../types/orderBy.type';
+import { ResponsiveTable } from '../ResponsiveTable/ResponsiveTable';
 
 type TaskListComponentProps = {
   paginatedTasks?: PaginatedResults<Task>;
@@ -65,32 +65,34 @@ export const TaskListComponent = ({
   const columns: Column<Task>[] = React.useMemo(
     () => [
       {
-        Header: "Title",
-        accessor: "title",
+        Header: 'Title',
+        accessor: 'title',
       },
       {
-        Header: "Description",
-        accessor: "description",
+        Header: 'Description',
+        accessor: 'description',
       },
       {
-        Header: "User",
+        Header: 'User',
         Cell: ({ row }: { row: Row<Task> }) => {
           const { user = null } = row.original;
           return user ? (
             <Link as={RLink} to={`/users/${user?.id}`}>
-              <VStack align="start" as="dl">
-                <HStack>
-                  <Text as="dt" fontWeight="bold">
+              <VStack align='start' as='dl'>
+                <Stack direction={['column', null, 'row']}>
+                  <Text as='dt' fontWeight='bold'>
                     Username:
                   </Text>
-                  <Text as="dd">{user?.username}</Text>
-                </HStack>
-                <HStack>
-                  <Text as="dt" fontWeight="bold">
+                  <Text as='dd'>{user?.username}</Text>
+                </Stack>
+                <Stack direction={['column', null, 'row']}>
+                  <Text as='dt' fontWeight='bold'>
                     Email:
                   </Text>
-                  <Text as="dd">{user?.email}</Text>
-                </HStack>
+                  <Text wordBreak='break-all' as='dd'>
+                    {user?.email}
+                  </Text>
+                </Stack>
               </VStack>
             </Link>
           ) : (
@@ -99,29 +101,29 @@ export const TaskListComponent = ({
         },
       },
       {
-        Header: "Actions",
-        id: "actions",
+        Header: 'Actions',
+        id: 'actions',
         Cell: ({ row }: { row: Row<Task> }) => {
           const { id } = row.original;
           return (
-            <ButtonGroup isAttached size="xs">
+            <ButtonGroup isAttached size='xs'>
               <Button
-                borderRightRadius="0"
+                borderRightRadius='0'
                 as={RLink}
                 to={`tasks/${id}`}
-                rounded="full"
-                colorScheme="green"
+                rounded='full'
+                colorScheme='green'
               >
                 Edit Task
               </Button>
               <Button
-                borderLeftRadius="0"
+                borderLeftRadius='0'
                 onClick={() => {
                   setCurrentTask(id);
                   setOpen(true);
                 }}
-                rounded="full"
-                colorScheme="red"
+                rounded='full'
+                colorScheme='red'
               >
                 Delete Task
               </Button>
@@ -175,7 +177,7 @@ export const TaskListComponent = ({
 
   const { mutate } = useMutation(mutationFn, {
     onSuccess: () => {
-      queryClient.invalidateQueries("tasks");
+      queryClient.invalidateQueries('tasks');
     },
     onSettled: () => {
       setOpen(false);
@@ -199,152 +201,153 @@ export const TaskListComponent = ({
   return (
     <>
       <Heading
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        as="h1"
-        mb="8"
+        display='flex'
+        justifyContent='space-between'
+        alignItems='center'
+        as='h1'
+        mb='8'
       >
         Tasks
         <Button
           as={RLink}
-          to="/tasks/create"
-          colorScheme="green"
-          rounded="full"
+          to='/tasks/create'
+          colorScheme='green'
+          rounded='full'
         >
           New Task
         </Button>
       </Heading>
-      <Box overflowX="scroll">
-        <Table {...getTableProps()} variant="simple" colorScheme="blackAlpha">
-          <Thead>
-            {headerGroups.map((headerGroup: HeaderGroup<Task>) => {
-              return (
-                <Tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => {
-                    return (
-                      <Th
-                        onClick={() => console.log("Hello")}
-                        {...column.getHeaderProps(
-                          column.getSortByToggleProps()
-                        )}
-                      >
-                        <Wrap as="div">
-                          <WrapItem as="div">
-                            {column.render("Header")}
-                          </WrapItem>
-                          <WrapItem as="div">
-                            {column.isSorted ? (
-                              column.isSortedDesc ? (
-                                <Icon as={FaChevronDown} w={4} h={4} />
-                              ) : (
-                                <Icon as={FaChevronUp} w={4} h={4} />
-                              )
+      <ResponsiveTable
+        reactTableProps={getTableProps()}
+        tableProps={{ variant: 'simple', colorScheme: 'blackAlpha' }}
+      >
+        <Thead>
+          {headerGroups.map((headerGroup: HeaderGroup<Task>) => {
+            return (
+              <Tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => {
+                  return (
+                    <Th
+                      onClick={() => console.log('Hello')}
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                    >
+                      <Wrap as='div'>
+                        <WrapItem as='div'>{column.render('Header')}</WrapItem>
+                        <WrapItem as='div'>
+                          {column.isSorted ? (
+                            column.isSortedDesc ? (
+                              <Icon as={FaChevronDown} w={4} h={4} />
                             ) : (
-                              ""
-                            )}
-                          </WrapItem>
-                        </Wrap>
-                      </Th>
-                    );
-                  })}
-                </Tr>
-              );
-            })}
-          </Thead>
-          <Tbody {...getTableBodyProps()}>
-            {page.map((row) => {
-              prepareRow(row);
-              return (
-                <Tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>
-                    );
-                  })}
-                </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
-        {totalPages > 1 && (
-          <Box mb="8">
-            <Paginated
-              currentPage={currentPage}
-              totalPage={totalPages}
-              siblingsSize={2}
-              boundarySize={2}
-            >
-              {({
-                pages,
-                currentPage,
-                hasPrev,
-                hasNext,
-                getFirstBoundary,
-                getLastBoundary,
-                isPrevTruncated,
-                isNextTruncated,
-              }) => (
-                <ButtonGroup
-                  mt="5"
-                  colorScheme="blue"
-                  variant="outline"
-                  isAttached={true}
-                >
-                  {hasPrev() && (
-                    <>
-                      <Button onClick={() => setPage(1)}>First</Button>
-                      <Button onClick={() => setPage((prev) => prev - 1)}>
-                        Prev
-                      </Button>
-                    </>
-                  )}
-                  {getFirstBoundary().map((boundary) => (
-                    <Button onClick={() => setPage(boundary)} key={boundary}>
-                      {boundary}
+                              <Icon as={FaChevronUp} w={4} h={4} />
+                            )
+                          ) : (
+                            ''
+                          )}
+                        </WrapItem>
+                      </Wrap>
+                    </Th>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <Td data-th={cell.column.Header} {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </ResponsiveTable>
+      {totalPages > 1 && (
+        <Box mb='8'>
+          <Paginated
+            currentPage={currentPage}
+            totalPage={totalPages}
+            siblingsSize={2}
+            boundarySize={2}
+          >
+            {({
+              pages,
+              currentPage,
+              hasPrev,
+              hasNext,
+              getFirstBoundary,
+              getLastBoundary,
+              isPrevTruncated,
+              isNextTruncated,
+            }) => (
+              <ButtonGroup
+                flexWrap='wrap'
+                mt='5'
+                colorScheme='blue'
+                variant='outline'
+                isAttached={true}
+              >
+                {hasPrev() && (
+                  <>
+                    <Button onClick={() => setPage(1)}>First</Button>
+                    <Button onClick={() => setPage((prev) => prev - 1)}>
+                      Prev
                     </Button>
-                  ))}
-                  {isPrevTruncated && <Button>...</Button>}
-                  {pages.map((page) => {
-                    return page === currentPage ? (
-                      <Button variant="solid" disabled={true} key={page}>
-                        {page}
-                      </Button>
-                    ) : (
-                      <Button
-                        onClick={() => {
-                          setPage(page);
-                        }}
-                        key={page}
-                      >
-                        {page}
-                      </Button>
-                    );
-                  })}
-                  {isNextTruncated && <Button>...</Button>}
-                  {getLastBoundary().map((boundary) => (
-                    <Button onClick={() => setPage(boundary)} key={boundary}>
-                      {boundary}
+                  </>
+                )}
+                {getFirstBoundary().map((boundary) => (
+                  <Button onClick={() => setPage(boundary)} key={boundary}>
+                    {boundary}
+                  </Button>
+                ))}
+                {isPrevTruncated && <Button>...</Button>}
+                {pages.map((page) => {
+                  return page === currentPage ? (
+                    <Button variant='solid' disabled={true} key={page}>
+                      {page}
                     </Button>
-                  ))}
-                  {hasNext() && (
-                    <>
-                      <Button
-                        onClick={() => {
-                          setPage((prev) => prev + 1);
-                        }}
-                      >
-                        Next
-                      </Button>
-                      <Button onClick={() => setPage(totalPages)}>Last</Button>
-                    </>
-                  )}
-                </ButtonGroup>
-              )}
-            </Paginated>
-          </Box>
-        )}
-      </Box>
+                  ) : (
+                    <Button
+                      onClick={() => {
+                        setPage(page);
+                      }}
+                      key={page}
+                    >
+                      {page}
+                    </Button>
+                  );
+                })}
+                {isNextTruncated && <Button>...</Button>}
+                {getLastBoundary().map((boundary) => (
+                  <Button onClick={() => setPage(boundary)} key={boundary}>
+                    {boundary}
+                  </Button>
+                ))}
+                {hasNext() && (
+                  <>
+                    <Button
+                      onClick={() => {
+                        setPage((prev) => prev + 1);
+                      }}
+                    >
+                      Next
+                    </Button>
+                    <Button onClick={() => setPage(totalPages)}>Last</Button>
+                  </>
+                )}
+              </ButtonGroup>
+            )}
+          </Paginated>
+        </Box>
+      )}
+
       <DeletionVerification
         alertProps={{
           isCentered: true,
@@ -352,8 +355,8 @@ export const TaskListComponent = ({
         }}
         isOpen={open}
         {...{ onClose, onDelete }}
-        title="Delete Task"
-        bodyText="Are you sure you want to delete the task?"
+        title='Delete Task'
+        bodyText='Are you sure you want to delete the task?'
       />
     </>
   );
